d97d96867c957e611edb2576baf29f68ca2eb36e
jm29 <jm29@70-12-243-19.pools.spcsdns.net>
2017-07-07 20:56:17 +0900

    Save first Sinatra

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+
+get '/' do
+	"hello"
+end
+
+get '/likelion/:name' do
+	"Hello, Lieklion, #{params[:name]}"
+end
+
+get '/view' do
+ send_file 'index.html'
+end
+
+get '/intro' do
+	erb :intro
+end
+
+get '/math/:num' do
+	num = params[:num].to_i
+	"#{num**3}"
+end
